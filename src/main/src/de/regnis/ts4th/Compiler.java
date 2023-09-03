1f4360ad0388048c250999e05df3ac65abf4f413
Thomas Singer <tmssngr@users.noreply.github.com>
2023-09-03 16:46:26 +0200

    Compiler: only handle used (needed) functions and ignore all others

--- src/main/src/de/regnis/ts4th/Compiler.java
+++ src/main/src/de/regnis/ts4th/Compiler.java
@@ -5,6 +5,8 @@ import java.nio.file.*;
 import java.util.*;
 import java.util.concurrent.*;
 
+import org.jetbrains.annotations.*;
+
 /**
  * @author Thomas Singer
  */
@@ -17,33 +19,84 @@ public class Compiler {
 		final TypeCheckerImpl typeChecker = new TypeCheckerImpl();
 		functions.forEach(function -> typeChecker.add(function.name(), function.typesInOut()));
 
-		for (Function function : functions) {
+		final List<Function> usedFunctions = getUsedFunctions(functions);
+
+		for (Function function : usedFunctions) {
 			final CfgFunction cfgFunction = new CfgFunction(function);
 			cfgFunction.checkTypes(typeChecker);
 		}
 
-		Inliner.process(functions);
+		Inliner.process(usedFunctions);
 
 		final AsmIRStringLiterals stringLiterals = new AsmIRStringLiterals();
 		final List<AsmIRFunction> processedFunctions = new ArrayList<>();
-		for (Function function : functions) {
+		for (Function function : usedFunctions) {
 			final AsmIRFunction irFunction = AsmIRConverter.convertToIR(function, typeChecker, stringLiterals);
 			processedFunctions.add(irFunction);
 		}
 
+		final Path asmFile = forthFile.resolveSibling(getAsmName(forthFile));
+		writeAsmFile(asmFile, processedFunctions, stringLiterals);
+
+		launchFasm(asmFile);
+	}
+
+	private static List<Function> getUsedFunctions(List<Function> functions) {
+		final List<Function> usedFunctions = new ArrayList<>();
+
+		final Set<String> invoked = new HashSet<>();
+		final Deque<String> pending = new ArrayDeque<>();
+		pending.add("main");
+		invoked.add("main");
+
+		while (!pending.isEmpty()) {
+			final String name = pending.removeFirst();
+			final Function function = getFunctionNamed(name, functions);
+			usedFunctions.add(function);
+
+			for (Instruction instruction : function.instructions()) {
+				final String command = instruction.getCommand();
+				if (command == null || BuiltinCommands.get(command) != null) {
+					continue;
+				}
+
+				if (invoked.add(command)) {
+					pending.add(command);
+				}
+			}
+		}
+
+		return usedFunctions;
+	}
+
+	private static Function getFunctionNamed(String name, List<Function> functions) {
+		for (Function function : functions) {
+			if (function.name().equals(name)) {
+				return function;
+			}
+		}
+		throw new CompilerException("no function `" + name + "` found");
+	}
+
+	private static void writeAsmFile(Path asmFile, List<AsmIRFunction> processedFunctions, AsmIRStringLiterals stringLiterals) throws IOException {
+		try (Writer writer = Files.newBufferedWriter(asmFile)) {
+			final X86Win64 x86Win64 = new X86Win64(writer);
+			x86Win64.write(processedFunctions, stringLiterals);
+		}
+	}
+
+	@NotNull
+	private static String getAsmName(Path forthFile) {
 		String name = forthFile.getFileName().toString();
 		int dotIndex = name.lastIndexOf('.');
 		if (dotIndex >= 0) {
 			name = name.substring(0, dotIndex);
 		}
 		name = name + ".asm";
-		final Path asmFile = forthFile.resolveSibling(name);
+		return name;
+	}
 
-		try (Writer writer = Files.newBufferedWriter(asmFile)) {
-			final X86Win64 x86Win64 = new X86Win64(writer);
-			x86Win64.write(processedFunctions, stringLiterals);
-		}
-
+	private static void launchFasm(Path asmFile) throws IOException, InterruptedException {
 		final String fasmHome = System.getenv("FASM_HOME");
 		final Path fasmExe = fasmHome != null ? Paths.get(fasmHome, "fasm.exe") : Paths.get("fasm.exe");
 
---
+++ src/main/src/de/regnis/ts4th/CompilerException.java
@@ -0,0 +1,11 @@
+package de.regnis.ts4th;
+
+/**
+ * @author Thomas Singer
+ */
+public class CompilerException extends RuntimeException {
+
+	public CompilerException(String message) {
+		super(message);
+	}
+}
