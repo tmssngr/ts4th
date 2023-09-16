11f2d569a419059e744d01c7f928ba05eef09b5a
Thomas Singer <tmssngr@users.noreply.github.com>
2023-09-16 15:57:57 +0200

    AsmIRSimplifierTest: import AsmIRFactory

--- src/main/src-test/de/regnis/ts4th/AsmIRSimplifierTest.java
+++ src/main/src-test/de/regnis/ts4th/AsmIRSimplifierTest.java
@@ -5,6 +5,7 @@ import org.junit.*;
 import java.util.*;
 
 import static de.regnis.ts4th.AsmIR.*;
+import static de.regnis.ts4th.AsmIRFactory.*;
 import static org.junit.Assert.*;
 
 /**
@@ -15,102 +16,102 @@ public class AsmIRSimplifierTest {
 	@Test
 	public void testRemoveIndirectLabel() {
 		testSimplify(List.of(
-				AsmIRFactory.label("loop"),
-				AsmIRFactory.call("foo"),
-				AsmIRFactory.jump(Condition.lt, "else"),
-				AsmIRFactory.literal(0),
-				AsmIRFactory.jump("loop"),
-				AsmIRFactory.label("else"),
-				AsmIRFactory.literal(1),
-				AsmIRFactory.jump("loop")
+				label("loop"),
+				call("foo"),
+				jump(Condition.lt, "else"),
+				literal(0),
+				jump("loop"),
+				label("else"),
+				literal(1),
+				jump("loop")
 		), List.of(
-				AsmIRFactory.label("loop"),
-				AsmIRFactory.call("foo"),
-				AsmIRFactory.jump(Condition.lt, "else"),
-				AsmIRFactory.literal(0),
-				AsmIRFactory.jump("end"),
-				AsmIRFactory.label("else"),
-				AsmIRFactory.literal(1),
-				AsmIRFactory.label("end"),
-				AsmIRFactory.jump("loop")
+				label("loop"),
+				call("foo"),
+				jump(Condition.lt, "else"),
+				literal(0),
+				jump("end"),
+				label("else"),
+				literal(1),
+				label("end"),
+				jump("loop")
 		));
 	}
 
 	@Test
 	public void testCommandAfterJump() {
 		testSimplify(List.of(
-				AsmIRFactory.jump(Condition.nz, "else"),
-				AsmIRFactory.jump("end"),
-				AsmIRFactory.label("else"),
-				AsmIRFactory.literal(0),
-				AsmIRFactory.label("end")
+				jump(Condition.nz, "else"),
+				jump("end"),
+				label("else"),
+				literal(0),
+				label("end")
 		), List.of(
-				AsmIRFactory.jump(Condition.nz, "else"),
-				AsmIRFactory.jump("end"),
-				AsmIRFactory.literal(2),
-				AsmIRFactory.literal(1),
-				AsmIRFactory.label("else"),
-				AsmIRFactory.literal(0),
-				AsmIRFactory.label("end")
+				jump(Condition.nz, "else"),
+				jump("end"),
+				literal(2),
+				literal(1),
+				label("else"),
+				literal(0),
+				label("end")
 		));
 	}
 
 	@Test
 	public void testJumpToNext() {
 		testSimplify(List.of(
-				AsmIRFactory.literal(1)
+				literal(1)
 		), List.of(
-				AsmIRFactory.jump("next"),
-				AsmIRFactory.label("next"),
-				AsmIRFactory.literal(1)
+				jump("next"),
+				label("next"),
+				literal(1)
 		));
 
 		testSimplify(List.of(
-				AsmIRFactory.literal(1)
+				literal(1)
 		), List.of(
-				AsmIRFactory.jump(Condition.nz, "next"),
-				AsmIRFactory.label("next"),
-				AsmIRFactory.literal(1)
+				jump(Condition.nz, "next"),
+				label("next"),
+				literal(1)
 		));
 	}
 
 	@Test
 	public void testRemoveUnusedLabels() {
 		testSimplify(List.of(
-				AsmIRFactory.literal(1)
+				literal(1)
 		), List.of(
-				AsmIRFactory.label("start"),
-				AsmIRFactory.literal(1)
+				label("start"),
+				literal(1)
 		));
 
 		testSimplifyExpectNoChange(List.of(
-				AsmIRFactory.jump(Condition.z, "else"),
-				AsmIRFactory.literal(1),
-				AsmIRFactory.jump("end"),
-				AsmIRFactory.label("else"),
-				AsmIRFactory.literal(2),
-				AsmIRFactory.label("end")
+				jump(Condition.z, "else"),
+				literal(1),
+				jump("end"),
+				label("else"),
+				literal(2),
+				label("end")
 		));
 	}
 
 	@Test
 	public void testPushPop() {
 		testSimplify(List.of(
-				AsmIRFactory.literal(1),
-				AsmIRFactory.call("print")
+				literal(1),
+				call("print")
 		), List.of(
-				AsmIRFactory.literal(1),
-				AsmIRFactory.push(1, 2),
-				AsmIRFactory.pop(1, 2),
-				AsmIRFactory.call("print")
+				literal(1),
+				push(1, 2),
+				pop(1, 2),
+				call("print")
 		));
 
 		try {
 			AsmIRSimplifier.simplify(List.of(
-					AsmIRFactory.literal(1),
-					AsmIRFactory.push(1, 2),
-					AsmIRFactory.pop(1, 1),
-					AsmIRFactory.call("print")
+					literal(1),
+					push(1, 2),
+					pop(1, 1),
+					call("print")
 			));
 			fail();
 		}
