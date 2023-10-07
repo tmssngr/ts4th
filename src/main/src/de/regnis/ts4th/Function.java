7e85b9802ff682a3e3def2f1757f25ad56930be7
Thomas Singer <tmssngr@users.noreply.github.com>
2023-10-07 17:28:45 +0200

    Function.printInstructions(List)

--- src/main/src/de/regnis/ts4th/Function.java
+++ src/main/src/de/regnis/ts4th/Function.java
@@ -7,6 +7,15 @@ import java.util.*;
  */
 public record Function(Location locationStart, Location locationEnd, String name, TypesInOut typesInOut, boolean isInline, List<Instruction> instructions) implements Declaration {
 
+	public static void printInstructions(List<Instruction> instructions) {
+		for (Instruction instruction : instructions) {
+			if (!(instruction instanceof Instruction.Label)) {
+				System.out.print("    ");
+			}
+			System.out.println(instruction);
+		}
+	}
+
 	public Function(String name, TypeList typeIn, TypeList typeOut, boolean isInline, List<Instruction> commands) {
 		this(Location.DUMMY, Location.DUMMY, name, new TypesInOut(typeIn, typeOut), isInline, commands);
 	}
@@ -17,11 +26,6 @@ public record Function(Location locationStart, Location locationEnd, String name
 	}
 
 	public void printInstructions() {
-		for (Instruction instruction : instructions) {
-			if (!(instruction instanceof Instruction.Label)) {
-				System.out.print("    ");
-			}
-			System.out.println(instruction);
-		}
+		printInstructions(instructions);
 	}
 }
